af68603906ca3b17d2cd23a81562c591fde1f7ab
Vlad-Andrei Erculescu <vlad.erculescu@hotmail.com>
2020-12-14 19:09:50 +0000

    Update g923_pedals.ino

--- g923_pedals.ino
+++ g923_pedals.ino
@@ -1,12 +1,8 @@
-int RXLED = 17; // The RX LED has a defined Arduino pin
-int TXLED = 30; // The TX LED has a defined Arduino pin
+// Frequency to update the pedals values at (100Hz -> 100 times a second). Must be between 10Hz and 100Hz
+int UPDATE_FREQUENCY_HZ = 50;
 
-int TIME = 0; // Time loop
-
-int STEP_DELAY = 50; // Loop step delay
-
-#include <Joystick.h>
-
+#include <Joystick.h> // Include required underlying Joystick library
+ 
 Joystick_ Joystick(
   JOYSTICK_DEFAULT_REPORT_ID, // Indicates the joystick's HID report ID (0x03)
   JOYSTICK_TYPE_MULTI_AXIS, // Multi-axis Controller (0x08)
@@ -25,55 +21,66 @@ Joystick_ Joystick(
   false // Indicates if the Steering is available on the joystick.
 );
 
-int Throttle_ = 0;
-int Brake_ = 0; 
-int Clutch_ = 0; 
-
-const bool initAutoSendState = true; 
+unsigned long LOOP_COUNT = 0; // Loop counter
+int RXLED = 17; // The RX LED has a defined Arduino pin
+int TXLED = 30; // The TX LED has a defined Arduino pin
 
 void setup()
-{
-  Joystick.begin();
+{  
+  // Set TXLED and RXLED as outputs
+  pinMode(TXLED, OUTPUT); //TXLED Will be turned off
+  pinMode(RXLED, OUTPUT); // RXLED Will blink depending on the update frequency
   
-  pinMode(TXLED, OUTPUT); // Set TX LED as an output
+  // Initialize joystick object with auto send false
+  Joystick.begin(false);
   
-  digitalWrite(TXLED, HIGH); // set the LED on
-  digitalWrite(RXLED, LOW); // set the LED on
+  // Set axis range
+  Joystick.setThrottleRange(0,1023);
+  Joystick.setBrakeRange(0,1023);
+  Joystick.setZAxisRange(0,1023);
+  
+  // Init axis values
+  Joystick.setThrottle(0);
+  Joystick.setBrake(0);
+  Joystick.setZAxis(0);
+  
+  // Validate update frequency setting
+  if (UPDATE_FREQUENCY_HZ > 100)
+  {
+    UPDATE_FREQUENCY_HZ = 100;
+  }
+  if (UPDATE_FREQUENCY_HZ < 10)
+  {
+    UPDATE_FREQUENCY_HZ = 10;
+  }
 }
 
 void loop(){
-  Throttle_ = analogRead(A2);
-  Throttle_ = map(Throttle_,1023,0,255,0);         
-  Joystick.setThrottle(Throttle_);
+  // Update Throttle value from pin A2
+  Joystick.setThrottle(analogRead(A2));
   
-  Brake_ = analogRead(A1);
-  Brake_ = map(Brake_,1023,0,255,0);         
-  Joystick.setBrake(Brake_);
+  // Update Brake value from pin A1
+  Joystick.setBrake(analogRead(A1));
   
-  Clutch_ = analogRead(A0);
-  Clutch_ = map(Clutch_,1023,0,255,0);         
-  Joystick.setZAxis(Clutch_);
+  // Update Clutch value from pin A0
+  Joystick.setZAxis(analogRead(A0));
+  
+  // Send updated values to host
+  Joystick.sendState();
   
   // If you don't want the leds to blink, delete the block between [LED_BLINK] and  [/LED_BLINK]
   // [LED _BLINK]
-  TIME += STEP_DELAY;
-  
-  if (TIME < 500)
-  {
-    digitalWrite(TXLED, LOW); // set the LED off
-    digitalWrite(RXLED, HIGH); // set the LED off
-  }
-  if (TIME >= 500)
-  {
-    digitalWrite(TXLED, HIGH); // set the LED off
-    digitalWrite(RXLED, LOW); // set the LED off
-  }
-  
-  if (TIME >= 1000)
-  {
-    TIME = 0;
-  }
+    if (LOOP_COUNT % 20 < 5)
+    { digitalWrite(RXLED, LOW); } // set the TXLED off
+    else
+    { digitalWrite(RXLED, HIGH); } // set the TXLED on
   // [/LED _BLINK]
   
-  delay(STEP_DELAY);
+  delay(1000 / UPDATE_FREQUENCY_HZ);
+  LOOP_COUNT++;
+  
+  // Reset LOOP counter to 0 after 5000 itterations to prevent it running out of memory
+  if (LOOP_COUNT > 5000) {
+    LOOP_COUNT = 0;
+  }
 }
